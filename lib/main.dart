e8a3c868c0da3c4895bcddc1a77335753199a5dc
vindicta43 <alperen.erdogan.neu@gmail.com>
2021-03-14 20:35:08 +0300

    models klasörü oluşturuldu ve veriler entegre edildi

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Burç Rehberi App",
       theme: ThemeData(
         primarySwatch: Colors.blue,
--- lib/screens/burc_listesi.dart
+++ lib/screens/burc_listesi.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_burc_rehberi/models/burc.dart';
+import 'package:flutter_burc_rehberi/utils/strings.dart';
 
 class BurcListesi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    List<Burc> tumBurclar = veriGetir();
+
+    return Scaffold(
+      appBar: AppBar(title: Text("Burç Rehberi")),
+      body: Container(),
+    );
+  }
+
+  List<Burc> veriGetir() {
+    List<Burc> burcFill = [];
+
+    // Getting length from BURC_ADLARI for making dynamic length
+    for (int i = 0; i < Strings.BURC_ADLARI.length; i++) {
+      // Converting path to string value
+      // Koc -> koc1.png
+      String kucukResim = "/images/${Strings.BURC_ADLARI[i].toLowerCase()}${i+1}.png";
+      // Koc -> koc_buyuk1.png
+      String buyukResim = "/images/${Strings.BURC_ADLARI[i].toLowerCase()}_buyuk${i+1}.png";
+
+      burcFill.add(Burc(
+          Strings.BURC_ADLARI[i],
+          Strings.BURC_TARIHLERI[i],
+          Strings.BURC_GENEL_OZELLIKLERI[i],
+          kucukResim,
+          buyukResim));
+    }
+    return burcFill;
   }
 }
